9828ba7c11b72e2769579e0df94be3e6d34ad8b3
nitishbali <nitishbali24@gmail.com>
2020-07-14 15:57:02 +0530

    Assignment changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import menuData from './menu-data.json';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import ProductDetail from "./ProductDetail";
+import menuData from "./menu-data.json";
 
-console.log('Menu Data');
-console.log(menuData);
+export interface IMenuItem {
+  available: boolean;
+  category: string;
+  description: string;
+  imageUrl: string;
+  itemId: number;
+  modifierGroups: object;
+  name: string;
+  price: number;
+  tax: number;
+  timePeriods: Array<object>;
+  [val: string]: any;
+}
+export interface IMenuData {
+  [val: string]: IMenuItem;
+}
+export interface IAppState {
+  menu: IMenuData;
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<any, IAppState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      menu: menuData,
+    };
+  }
+
+  changeValues = (value: string, name: string, itemId: string) => {
+    const { menu } = this.state;
+    let tempMenu: IMenuData = { ...menu };
+    tempMenu[itemId][name] = value;
+    this.setState({ menu: tempMenu });
+    console.log("New Menu: ", tempMenu);
+  };
+
+  render() {
+    const { menu } = this.state;
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route
+            path="/:itemId"
+            render={() => (
+              <ProductDetail data={menu} changeHandler={this.changeValues} />
+            )}
+          ></Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
